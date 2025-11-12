1e7f5b1bcdb16014d2e4293584b5f0b5ddb3ea60
Yiiii0 <yiqiaozhou@outlook.com>
2025-11-11 21:12:45 -0500

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://yiiii0.github.io',
-  base: '/yiqiao_digital_home',
+  site: 'https://yiqiaoz.me',
+  // base: '/', // 自定义域名用户页不需要 base，默认就是根目录
   vite: {
     plugins: [tailwindcss()],
   },
